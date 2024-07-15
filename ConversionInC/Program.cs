33be24dbc9aa07e6455fc394a106490fb49445e9
frost <ronniecse8@gmail.com>
2024-07-15 22:14:24 +0530

    C# Conversion and Evaluation - 15-07-2024

---
+++ ConversionInC/Program.cs
@@ -0,0 +1,29 @@
+﻿//Implicit casting 
+int myInt = 9;
+double myDouble = myInt;       // Automatic casting: int to double
+
+Console.WriteLine(myInt);      // Outputs 9
+Console.WriteLine(myDouble);   // Outputs 9
+
+//Explicit Casting
+double myDouble3 = 9.78;
+int myInt3 = (int)myDouble;    // Manual casting: double to int
+
+Console.WriteLine(myDouble3);   // Outputs 9.78
+Console.WriteLine(myInt3);      // Outputs 9
+
+//Type Conversion Method
+int myInt2 = 10;
+double myDouble2 = 5.25;
+bool myBool = true;
+
+Console.WriteLine(Convert.ToString(myInt2));    // convert int to string
+Console.WriteLine(Convert.ToDouble(myInt2));    // convert int to double
+Console.WriteLine(Convert.ToInt32(myDouble2));  // convert double to int
+Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+
+//implicitely typed variable 
+var myNumber = 13;
+Console.WriteLine(myNumber.GetType());
+
+Console.ReadKey();
---
+++ OrderOfEvaluationC/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        // Order of evaluation
+        int result = a + b * c; // Multiplication is performed first, then addition
+        Console.WriteLine("Result of a + b * c: " + result);
+
+        // Using parentheses to change the order
+        result = (a + b) * c; // Addition is performed first, then multiplication
+        Console.WriteLine("Result of (a + b) * c: " + result);
+
+        // Another example with different operators
+        int x = 5;
+        int y = 10;
+        int z = 15;
+
+        int result2 = x + y - z / x; // Division is performed first, then addition, then subtraction
+        Console.WriteLine("Result of x + y - z / x: " + result2);
+
+        // Using parentheses to change the order
+        result2 = (x + y - z) / x; // Addition and subtraction are performed first, then division
+        Console.WriteLine("Result of (x + y - z) / x: " + result2);
+
+        // Demonstrating short-circuit evaluation with logical operators
+        bool cond1 = true;
+        bool cond2 = false;
+
+        bool result3 = cond1 || Method1(); // Method1() is not called because cond1 is true
+        Console.WriteLine("Result of cond1 || Method1(): " + result3);
+
+        bool result4 = cond2 && Method1(); // Method1() is not called because cond2 is false
+        Console.WriteLine("Result of cond2 && Method1(): " + result4);
+
+        Console.ReadKey();
+    }
+
+    static bool Method1()
+    {
+        Console.WriteLine("Method1() was called.");
+        return true;
+    }
+}
