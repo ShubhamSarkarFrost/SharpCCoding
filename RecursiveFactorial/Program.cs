798cf50c8b0b70441d0887835d79a8f42a250ec1
frost <ronniecse8@gmail.com>
2024-08-09 20:46:01 +0530

    Recursive Factorial + Recursive Fibonacci - 09-08-2024 - #090824-20-45

---
+++ RecursiveFactorial/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+int factorial = Factorial(number);
+Console.WriteLine($"Factorial of {number} is: {factorial}");
+
+
+static int Factorial(int n)
+{
+    // Base case: factorial of 0 or 1 is 1
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+
+    // Recursive case: n * factorial of (n-1)
+    return n * Factorial(n - 1);
+}
