c5b467f335c8f6ac3713bdd70ea3d9bd39256e30
frost <ronniecse8@gmail.com>
2025-01-15 01:52:18 +0530

    C # Basics Cont - 15-01-2025

---
+++ Practice_One_List_with_Loop/Program.cs
@@ -0,0 +1,8 @@
+﻿List<int> numbers = new List<int> { 1,2,3,4,5 };
+
+foreach (int number in numbers)
+{
+    Console.Write(number + " ");
+}
+
+Console.ReadKey();
---
+++ SortingListProg/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace SortingListProg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            // take data from the User
+            Console.WriteLine("Enter the Data in String Value in the List {Type done to Finish}");
+            List<string> newList = new List<string>();
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input.ToLower() == "done")
+                    break;
+                newList.Add(input);
+            }
+
+            // take the input of sorting method from the user
+            Console.WriteLine("Please enter the sorting method asc - Ascending or dsc - decending");
+            string sorting_method = Console.ReadLine()?.ToLower();
+
+            if(sorting_method == "asc")
+            {
+                newList = newList.OrderBy(s => s).ToList();
+            }else if(sorting_method == "dsc")
+            {
+                newList = newList.OrderByDescending(s => s).ToList();
+            }
+            else
+            {
+                Console.WriteLine("Invalid sorting method. Defaulting to ascending order.");
+                newList = newList.OrderBy(s => s).ToList();
+            }
+
+            // Step 4: Print the sorted list
+            Console.WriteLine("Sorted list:");
+            foreach (var str in newList)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+    
+}
